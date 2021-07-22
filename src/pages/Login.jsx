785dfc28f5716c8b2287c594931d06c5ff5a3b12
Fabian Ferno <fabianferno@gmail.com>
2021-07-23 03:46:11 +0530

    Add github integration

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,8 @@ import "../firebase";
 import firebase from "firebase/app";
 import "firebase/auth";
 
+import { SocialIcon } from "react-social-icons";
+
 export default function Login() {
   const history = useHistory();
   firebase.auth().onAuthStateChanged(function (user) {
@@ -31,21 +33,56 @@ export default function Login() {
       });
   }
 
+  function signInWithGitHub() {
+    var provider = new firebase.auth.GithubAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        //var credential = result.credential;
+
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        //var token = credential.accessToken;
+
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        // var errorCode = error.code;
+        // var errorMessage = error.message;
+      });
+  }
+
   return (
     <section>
       <div
         className="container d-flex align-items-center justify-content-center flex-column"
         style={{ minHeight: "100vh" }}
       >
-        <h1 className="text-white">
+        <h1 className="text-white bg-primary container text-center p-5 ">
           react-<strong>dump</strong>-tabs
         </h1>
-        <button
-          className="mt-3 btn btn-lg btn-primary container text-capitalize"
-          onClick={signInWithGoogle}
-        >
-          Sign in with Google
-        </button>
+        <p className="text-white bg-dark container text-center p-2 ">
+          Sign in below to start
+        </p>
+
+        <div className="d-flex justify-content-around mt-5">
+          <div className="mx-3" onClick={signInWithGitHub}>
+            <SocialIcon network="github" fgColor="#ffffff" bgColor="#3500D3" />
+            <span className="btn social-pill mr-5 font-weight-bold text-capitalize">
+              Github
+            </span>
+          </div>
+
+          <div className="mx-3" onClick={signInWithGoogle}>
+            <SocialIcon network="google" fgColor="#ffffff" bgColor="#3500D3" />
+            <span className="btn social-pill mr-5 font-weight-bold">
+              Google
+            </span>
+          </div>
+        </div>
       </div>
     </section>
   );
