d79d5c5e893e49ac0525e77da7405e902b135dde
Fabian Ferno <fabianferno@gmail.com>
2021-07-23 13:38:53 +0530

    Place it better

--- src/components/dumps/Zone.jsx
+++ src/components/dumps/Zone.jsx
@@ -13,49 +13,53 @@ export default function Zone({
     root: {
       width: 300,
       "& .MuiInputBase-root": {
-        color: "#3500D3",
+        color: "#000000",
       },
     },
   }))(TextField);
 
   return (
     <div className="part-container">
-      <h1 style={{ zIndex: "1" }} className="text-white no-select mx-4">
-        {title}
-      </h1>
-
       <motion.div className="overlay  " />
       {isSelected && (
-        <motion.div
-          className="box container"
-          layoutId="box"
-          initial={false}
-          animate={{ backgroundColor: color }}
-          drag
-          // Snap the box back to its center when we let go
-          dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
-          // Allow full movememnt outside constraints
-          dragElastic={1}
-          onViewportBoxUpdate={onViewportBoxUpdate}
-        >
-          <form action="#" className="p-2 text-white ">
-            <StyledTextField
-              label="Type something..."
-              rows={2}
-              fullWidth
-              multiline
-              variant="filled"
-              className="d-flex"
-              // onChange={handleChange}
-              // value={state.note}
-            />
-            <div className="d-flex justify-content-end">
-              <button className="mt-2 badge rounded-pill btn  btn-block btn-primary">
-                + Add a new dump
-              </button>
-            </div>
-          </form>
-        </motion.div>
+        <div className="">
+          <h1
+            style={{ zIndex: "1" }}
+            className="text-white no-select mx-4 mb-3"
+          >
+            {title}
+          </h1>
+          <motion.div
+            className="box container  "
+            layoutId="box"
+            initial={false}
+            animate={{ backgroundColor: color }}
+            drag
+            // Snap the box back to its center when we let go
+            dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+            // Allow full movememnt outside constraints
+            dragElastic={1}
+            onViewportBoxUpdate={onViewportBoxUpdate}
+          >
+            <form action="#" className="p-2 text-white ">
+              <StyledTextField
+                label="Type something..."
+                rows={2}
+                fullWidth
+                multiline
+                variant="filled"
+                className="d-flex"
+                // onChange={handleChange}
+                // value={state.note}
+              />
+              <div className="d-flex justify-content-end">
+                <button className="mt-2 badge rounded-pill btn   btn-block btn-primary">
+                  + Add a new dump
+                </button>
+              </div>
+            </form>
+          </motion.div>
+        </div>
       )}
     </div>
   );
